1774e3707088fc6f6e9c96965c20ff3ccf3013d3
RATAN23 <rnghasti42@gmail.com>
2024-06-11 23:49:50 +0530

    Updated readme.md file again

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,70 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Carousel from './components/carousel'
+/* Build a Highly Scalable Carousel Component in React JS
 
+ Requirements:
+  - We want to create a carousel component which takes array of images as input.
+  - The component should efficiently handle a large number of images while maintaining 
+  scalability, performance optimizations, and extensibility.
+  - Provide callback functions for events like image click, enabling users to define 
+  custom behavior.
+  - Focus on Accessibility.
+*/
 
-function App() {
-  const [loading, setLoading] =  useState(false);
-  const [images , setImages] =  useState([]);
-  // console.log(images);
+import {useEffect, useState} from "react";
+import "./App.css";
+import Carousel from "./components/carousel";
 
-  const fetchimages = async(imgLimit) => {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchImages = async (imgLimit) => {
     setLoading(true);
-    try{
+    try {
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/photos?_limit=${imgLimit}`
       );
       const data = await response.json();
       setImages(data);
-    }
-    catch(e){
-        console.log("Error fetching images: " + error );
-    } finally{
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    } finally {
       setLoading(false);
     }
   };
 
-  useEffect(() =>{
-      fetchimages(9);
-  },[]);
+  useEffect(() => {
+    fetchImages(8);
+  }, []);
 
   return (
-    <>
-    <div className='h-1/3 w-1/2'>
-      <Carousel 
-     images={images}
-     isLoading={loading}
-     onImgClick={(image, index) => {}}
-     imgPerSlide={2}
-     imageLimit={4}
-     customPrevButton={(onClick) => (
-       <button
-         className="btn prev"
-         style={{background: "red"}}
-         onClick={onClick}
-       >
-         Prev
-       </button>
-     )}
-     customNextButton={(onClick) => (
-       <button
-         className="btn next"
-         style={{background: "blue"}}
-         onClick={onClick}
-       >
-         Next
-       </button>
-     )}
+    <div className="carousel-container">
+      <Carousel
+        images={images}
+        isLoading={loading}
+        onImgClick={(image, index) => {}}
+        imgPerSlide={2}
+        imageLimit={4}
+        customPrevButton={(onClick) => (
+          <button
+            className="btn prev"
+            style={{background: "red"}}
+            onClick={onClick}
+          >
+            Prev
+          </button>
+        )}
+        customNextButton={(onClick) => (
+          <button
+            className="btn next"
+            style={{background: "blue"}}
+            onClick={onClick}
+          >
+            Next
+          </button>
+        )}
       />
     </div>
-      
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
